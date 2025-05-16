15af8930ccad0b307b2670d42a727d0ec4cc9607
tasbiha60 <htjtasbi@gmail.com>
2025-05-16 20:16:00 +0600

    Create codes.cpp
    patter printing in reverse

---
+++ codes.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int star=n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=star;j++){
+            printf("*");
+
+        }
+        star--;
+        printf("\n");
+    }
+    return 0;
+}
